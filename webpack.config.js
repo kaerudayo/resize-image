3876c1755af4c78d976421f185158752a7c81306
KaoruIto76 <soccerxxkaeru@gmail.com>
2021-11-24 20:37:04 +0900

    🐸 initial commit

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development', // "production" | "development" | "none"
+  entry: './src/index.ts',
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "index.js"
+  },
+  module: {
+    rules: [{
+      test: /\.ts$/,
+      use: 'ts-loader'
+    }]
+  },
+  resolve: {
+    modules: [
+      "node_modules",
+    ],
+    extensions: [
+      '.ts',
+      '.js'
+    ]
+  }
+};
